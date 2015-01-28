161943223402b5d0e9305fd4f772e99f124e23a8
wsl <aiai373824745@gmail.com>
2015-01-28 17:22:19 +0800

    Add cache file for universal-image-loader, asset add friend api success

--- src/com/android/cycling/CyclingApplication.java
+++ src/com/android/cycling/CyclingApplication.java
@@ -1,11 +1,16 @@
 package com.android.cycling;
 
+import java.io.File;
+
+import cn.bmob.im.BmobChat;
 import cn.bmob.v3.Bmob;
 
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.utils.StorageUtils;
 
 import android.app.Application;
 import android.content.Context;
@@ -18,6 +23,7 @@ public class CyclingApplication extends Application{
 		
 		initImageLoader(getApplicationContext());
 		Bmob.initialize(this, CyclingConfig.BMOB_APPLICATION_ID);
+		BmobChat.getInstance(getApplicationContext()).init(CyclingConfig.BMOB_APPLICATION_ID);
 	}
 	
 	public static void initImageLoader(Context context) {
@@ -25,9 +31,11 @@ public class CyclingApplication extends Application{
         // or you can create default configuration by
         //  ImageLoaderConfiguration.createDefault(this);
         // method.
+		File cacheDir = StorageUtils.getOwnCacheDirectory(context, "imageloader/Cache");
         ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
                 .threadPriority(Thread.NORM_PRIORITY - 2)
                 .denyCacheImageMultipleSizesInMemory()
+                .diskCache(new UnlimitedDiscCache(cacheDir))
                 .diskCacheFileNameGenerator(new Md5FileNameGenerator())
                 .diskCacheSize(50 * 1024 * 1024) // 50 Mb
                 .tasksProcessingOrder(QueueProcessingType.LIFO)
